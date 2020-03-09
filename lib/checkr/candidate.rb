3fa14634c9a0027ac25c7fab20e1c138dac4bad5
Catalin Ionescu <catalin.ionescu282@gmail.com>
2020-03-09 12:44:45 +0200

    Add a #find_by and #where finders for Checkr::Candidate

--- lib/checkr/candidate.rb
+++ lib/checkr/candidate.rb
@@ -2,6 +2,8 @@
 module Checkr
   class Candidate < Checkr::Base
 
+    ArgumentError = Class.new RuntimeError
+
     def self.create(params={})
       if valid?(params)
         uri = "/subaccounts/#{params[:subaccount_id]}" unless params[:subaccount_id].nil?
@@ -27,6 +29,37 @@ module Checkr
  	    end
 
  	    return candidate
+    end
+
+    PERMITTED_KEYS = %w(email full_name adjudication custom_id created_after created_before geo_id program_id)
+    ###
+    # Implements https://docs.checkr.com/#operation/listOfCandidates
+    # Accepted keys:
+    #   * email
+    #   * full_name
+    #   * adjudication
+    #   * custom_id
+    #   * created_after
+    #   * created_before
+    #   * geo_id
+    #   * program_id
+    ###
+    def self.where(params)
+      if params.is_a?(Hash)
+        unsupported_keys = params.stringify_keys.keys - PERMITTED_KEYS
+        if unsupported_keys.any?
+          raise ArgumentError, "Unsupported parameter(s) #{unsupported_keys.inspect}"
+        end
+      else
+        raise ArgumentError, 'Arguments must be a hash'
+      end
+
+      response = self.get("/candidates?#{params.to_query}", basic_auth: { username: Checkr.api_key } )
+      handle_response(response).data.map { |candidate| self.new candidate }
+    end
+
+    def self.find_by(params)
+      where(params).first
     end
 
     protected
